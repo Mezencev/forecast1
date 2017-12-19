7d1322540aa4c5631f455c9753b5f12478f1c81e
mezencev <mezencev20@rambler.ru>
2017-12-19 18:28:52 +0200

    add code

--- server.js
+++ server.js
@@ -9,30 +9,30 @@ app.use(bodyParse.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
-
 app.get('/', (req, res) => {
   res.render('inde', { weather: null, error: null });
 });
+
 app.post('/', (req, res) => {
-  res.render('inde');
   const city = req.body.city;
-  const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${Key.key}`;
+  const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${Key.key}`;
+
   request(url, (err, response, body) => {
-    if
-    (err) {
-      res.render('inde', { weather: null, err: 'Error, please try again' });
+    if (err) {
+      res.render('inde', { weather: null, error: 'Error, please try again' });
     } else {
       const weather = JSON.parse(body);
-      if
-      (weather.name === undefined) {
-        res.render('inde', { weather: null, err: 'Error, please try again' });
+      if (weather.main === undefined) {
+        res.render('inde', { weather: null, error: 'Error, please try again' });
       } else {
-        const weatherText = `it is ${weather.main.temp} degress in ${weather.name} !`;
-        res.render('inde', { weather: weatherText, err: null });
+        const weatherText = `It's ${weather.main.temp} degrees in ${weather.name}!
+        ${weather.weather[0].main}`;
+        res.render('inde', { weather: weatherText, error: null });
       }
     }
   });
 });
 
-app.listen(8080);
-
+app.listen(8080, () => {
+  console.log('Example app listening on port 3000!');
+});
