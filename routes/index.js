ede192f3a8410ae75f3a6638ab422f8828f2cd9f
mezencev <mezencev20@rambler.ru>
2017-12-20 22:16:45 +0200

    add key

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express();
+const request = require('request');
+const bodyParse = require('body-parser');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+router.use(bodyParse.urlencoded({ extended: true }));
+router.set('view engine', 'ejs');
+
+router.get('/', (req, res) => {
+  res.render('index', { weather: null, error: null });
 });
+
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,29 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express();
+const request = require('request');
+const bodyParse = require('body-parser');
+require('dotenv/config');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.set('view engine', 'ejs');
+router.use(bodyParse.urlencoded({ extended: true }));
+
+router.post('/', (req, res) => {
+  const city = req.body.city;
+  const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${process.env.API}`;
+  request(url, (err, response, body) => {
+    if (err) {
+      res.render('index', { weather: null, error: 'Error, please try again' });
+    } else {
+      const weather = JSON.parse(body);
+      if (weather.main === undefined) {
+        res.render('index', { weather: null, error: 'Erro, please try again' });
+      } else {
+        const weatherText = `It's ${weather.main.temp} degrees in ${weather.name}!
+        ${weather.weather[0].main}`;
+        res.render('index', { weather: weatherText, error: null });
+      }
+    }
+  });
 });
 
 module.exports = router;
