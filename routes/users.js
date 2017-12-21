3262f1468fae12ac1b0489b3ded543ec923c3b7f
mezencev <mezencev20@rambler.ru>
2017-12-21 15:41:51 +0200

    env

--- routes/users.js
+++ routes/users.js
@@ -4,19 +4,20 @@ const request = require('request');
 const bodyParse = require('body-parser');
 require('dotenv/config');
 
+router.use(express.static('public'));
 router.set('view engine', 'ejs');
 router.use(bodyParse.urlencoded({ extended: true }));
 
 router.post('/', (req, res) => {
   const city = req.body.city;
-  const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${process.env.API}`;
+  const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${process.env.KEY}`;
   request(url, (err, response, body) => {
     if (err) {
       res.render('index', { weather: null, error: 'Error, please try again' });
     } else {
       const weather = JSON.parse(body);
       if (weather.main === undefined) {
-        res.render('index', { weather: null, error: 'Erro, please try again' });
+        res.render('index', { weather: null, error: 'new error' });
       } else {
         const weatherText = `It's ${weather.main.temp} degrees in ${weather.name}!
         ${weather.weather[0].main}`;
